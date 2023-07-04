104480eb2252f05158ce4cd456ff2d9dac692919
Mariaps11 <beyordscene@gmail.com>
2023-07-04 14:16:19 +0300

    добавлено дз

--- homework/informationAboutYourself.cs
+++ homework/informationAboutYourself.cs
@@ -1,6 +1,4 @@
-﻿
-// Считываем информацию о себе
-Console.WriteLine("Введите информацию о себе:");
+﻿Console.WriteLine("Введите информацию о себе:");
 Console.Write("ФИО: ");
 string fullName = Console.ReadLine();
 
@@ -15,7 +13,6 @@ string github = Console.ReadLine();
 
 Console.WriteLine();
 
-// Выводим информацию о себе с отступами
 string FIO = "ФИО: ";
 string Addresses = "Адреса:";
 string Email = "Email:";
@@ -28,18 +25,3 @@ Console.WriteLine($"{Age,0}{age,0}");
 Console.WriteLine($"{Addresses}");
 Console.WriteLine($"{Email, 7}{email}");
 Console.WriteLine($"{GitHub, 8}{github}");
-
-/*
- * 
- * Console.WriteLine("Информация о пользователе:");
-Console.WriteLine($"{FIO, 0}{fullName, 0}");
-Console.WriteLine($"{Age, 0}{age, 0}");
-Console.WriteLine($"{Addresses}");
-Console.WriteLine($"{Email, 7}{email, 0}");
-Console.WriteLine($"{GitHub, 8}{github, 0}");
-string name = "Tom";
-int age = 23;
-
-Console.WriteLine($"Имя: {name,-5} Возраст: {age}"); // пробелы после
-Console.WriteLine($"Имя: {name,5} Возраст: {age}"); // пробелы до
-*/
