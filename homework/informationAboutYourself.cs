b1b82e56dc75b85be2260260c4255de28b846c49
Mariaps11 <beyordscene@gmail.com>
2023-07-04 14:14:35 +0300

    добавлено дз

---
+++ homework/informationAboutYourself.cs
@@ -0,0 +1,45 @@
+﻿
+// Считываем информацию о себе
+Console.WriteLine("Введите информацию о себе:");
+Console.Write("ФИО: ");
+string fullName = Console.ReadLine();
+
+Console.Write("Возраст: ");
+int age = int.Parse(Console.ReadLine());
+
+Console.Write("Email: ");
+string email = Console.ReadLine();
+
+Console.Write("GitHub: ");
+string github = Console.ReadLine();
+
+Console.WriteLine();
+
+// Выводим информацию о себе с отступами
+string FIO = "ФИО: ";
+string Addresses = "Адреса:";
+string Email = "Email:";
+string GitHub = "GitHub:";
+string Age = "Возраст: ";
+
+Console.WriteLine("Информация о пользователе:");
+Console.WriteLine($"{FIO, 0}{fullName, 0}");
+Console.WriteLine($"{Age,0}{age,0}");
+Console.WriteLine($"{Addresses}");
+Console.WriteLine($"{Email, 7}{email}");
+Console.WriteLine($"{GitHub, 8}{github}");
+
+/*
+ * 
+ * Console.WriteLine("Информация о пользователе:");
+Console.WriteLine($"{FIO, 0}{fullName, 0}");
+Console.WriteLine($"{Age, 0}{age, 0}");
+Console.WriteLine($"{Addresses}");
+Console.WriteLine($"{Email, 7}{email, 0}");
+Console.WriteLine($"{GitHub, 8}{github, 0}");
+string name = "Tom";
+int age = 23;
+
+Console.WriteLine($"Имя: {name,-5} Возраст: {age}"); // пробелы после
+Console.WriteLine($"Имя: {name,5} Возраст: {age}"); // пробелы до
+*/
