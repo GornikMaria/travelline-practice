fb650673308d879b8af6943f890b52013b686ccd
Mariaps11 <beyordscene@gmail.com>
2023-07-26 15:47:28 +0300

    исправления по N и по кодировке

--- homework3/SQLQuery4.sql
+++ homework3/SQLQuery4.sql
@@ -26,9 +26,9 @@ CREATE TABLE Car (
 
 --3.1 Написать INSERT на каждую таблицу
 -- Client
-INSERT INTO Client (Birthday, Name, Address) VALUES ('2020-12-12', 'Àëåêñàíäð', 'Óñïåíñêàÿ, ä.13');
-INSERT INTO Client (Birthday, Name, Address) VALUES ('1940-02-02', 'Âëàäèìèð', 'Êðàéîâñêàÿ, ä.42');
-INSERT INTO Client (Birthday, Name, Address) VALUES ('2001-08-04', 'Ïåòð', 'Óëè÷íàÿ, ä.2');
+INSERT INTO Client (Birthday, Name, Address) VALUES ('2020-12-12', N'Александр', N'Октябрьская, д.13');
+INSERT INTO Client (Birthday, Name, Address) VALUES ('1940-02-02', N'Владимир', N'Первомайская, д.42');
+INSERT INTO Client (Birthday, Name, Address) VALUES ('2001-08-04', N'Иван', N'Советская, д.2');
 
 -- BookingData
 INSERT INTO BookingData (IdClient, StartDate, EndDate, Pay) VALUES (3, '2014-02-02', '2014-02-03', 222);
@@ -41,17 +41,17 @@ INSERT INTO Car (IdClient, CreationDate, Name, Price, Rating) VALUES (2, 'Honda 
 INSERT INTO Car (IdClient, CreationDate, Name, Price, Rating) VALUES (3, 'BMW X5', '1980-02-02', 4123123, 3);
 
 -- 3.2 SELECT (используя каждый рассмотренный на паре предикат, + пару логических операций (and, or, not, ...), не менее 8 штук)
-SELECT * FROM Client WHERE Name = 'Àëåêñàíäð';
-SELECT * FROM Client WHERE Address != 'Ìîëîäåæíàÿ, ä. 5';
+SELECT * FROM Client WHERE Name = N'Александр';
+SELECT * FROM Client WHERE Address != N'Советская, д.2';
 SELECT * FROM BookingData WHERE Pay > 10;
 SELECT * FROM BookingData WHERE StartDate BETWEEN '2014-01-01' AND '2014-03-03';
 SELECT * FROM Car WHERE Rating IN (3);
-SELECT * FROM Client WHERE Name LIKE '%åò%';
+SELECT * FROM Client WHERE Name LIKE N'%ва%';
 SELECT * FROM Car WHERE Price > 30000 AND Rating = 3;
-SELECT * FROM Client WHERE Name = 'Ïåòð' OR Name = 'Àëåêñàíäð';
+SELECT * FROM Client WHERE Name = N'Иван' OR Name = N'Александр';
 
 -- 3.3 UPDATE
-UPDATE Client SET Address = 'Ìîëîäåæíàÿ, ä. 5' WHERE IdClient = 1;
+UPDATE Client SET Address = N'Советская, д.2' WHERE IdClient = 1;
 UPDATE BookingData SET EndDate = '2014-03-25' WHERE Pay = 230;
 UPDATE Car SET Price = 50000 WHERE Name LIKE '%Toyota%';
 
