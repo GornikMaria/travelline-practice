f4a3c3236a6ccd9717eda6e792d7c9c1f33d77b1
Mariaps11 <beyordscene@gmail.com>
2023-07-30 22:57:18 +0300

    расположить "рутовом" Index-файле

--- homework5/lw5/Pages/Index.cshtml.cs
+++ homework5/lw5/Pages/Index.cshtml.cs
@@ -1,20 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
-namespace lw5.Pages
+namespace lw5.Pages.Arithmetic
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public long Number1 { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public long Number2 { get; set; }
+
+        [BindProperty]
+        public string Operator { get; set; }
+
+        public SelectList OperatorList { get; } = new SelectList(new[]
         {
-            _logger = logger;
-        }
+            new { Value = "add", Text = "+" },
+            new { Value = "subtract", Text = "-" }
+        }, "Value", "Text");
+
+        public string Result { get; private set; }
 
         public void OnGet()
         {
+            // Пустой обработчик для HTTP GET запроса
+        }
 
+        public void OnPost()
+        {
+            if (Operator == "add")
+            {
+                Result = (Number1 + Number2).ToString();
+            }
+            else if (Operator == "subtract")
+            {
+                Result = (Number1 - Number2).ToString();
+            }
+        }
+
+        public List<int> RememberedValues { get; set; }
+        public IndexModel()
+        {
+            // Инициализируем список RememberedValues
+            RememberedValues = new List<int>();
         }
     }
 }
