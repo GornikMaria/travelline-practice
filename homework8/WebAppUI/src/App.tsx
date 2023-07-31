b78c884377ce5c72f8cb0de40999092007659086
Mariaps11 <beyordscene@gmail.com>
2023-07-31 15:51:11 +0300

    поправки

--- homework8/WebAppUI/src/App.tsx
+++ homework8/WebAppUI/src/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import CurrencyConverterForm from './CurrencyConverterForm';
 
-// Определение интерфейса для валюты
 interface Currency {
   code: string;
   name: string;
@@ -12,7 +11,6 @@ const App: React.FC = () => {
   const [currencies, setCurrencies] = useState<Currency[]>([]);
 
   useEffect(() => {
-    // Функция для получения списка валют с сервера
     const fetchCurrencies = async () => {
       try {
         const response = await axios.get<Currency[]>('http://localhost:5000/currencies');
@@ -21,8 +19,6 @@ const App: React.FC = () => {
         console.error('Ошибка при получении списка валют:', error);
       }
     };
-
-    // Вызываем функцию для получения списка валют
     fetchCurrencies();
   }, []);
 
--- homework8/WebAppUI/src/CurrencyConverterForm.tsx
+++ homework8/WebAppUI/src/CurrencyConverterForm.tsx
@@ -1,104 +1,78 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios, { Axios, AxiosResponse } from 'axios';
 
-// Интерфейс для валюты
 interface Currency {
   code: string;
   name: string;
 }
 
-// Интерфейс для пропсов компонента
 interface CurrencyConverterFormProps {
   currencies: Currency[];
 }
 
-// Создаем компонент CurrencyConverterForm, принимающий пропс currencies типа CurrencyConverterFormProps.
 const CurrencyConverterForm: React.FC<CurrencyConverterFormProps> = ({ currencies }) => {
+  const [sourceAmount, setSourceAmount] = useState<string>('0');
+  const [targetAmount, setTargetAmount] = useState<number>(0);
+  const [sourceCurrency, setSourceCurrency] = useState<Currency>(currencies[0]);
+  const [targetCurrency, setTargetCurrency] = useState<Currency>(currencies[1]);
+  const [exchangeRate, setExchangeRate] = useState<number>(0.011);
 
-  // Состояние компонента:
-  const [sourceAmount, setSourceAmount] = useState<string>('0'); // Состояние для значения исходной суммы в виде строки.
-  const [targetAmount, setTargetAmount] = useState<number>(0);  // Состояние для значения целевой суммы в виде числа.
-  const [sourceCurrency, setSourceCurrency] = useState<Currency>(currencies[0]); // Состояние для выбранной исходной валюты.
-  const [targetCurrency, setTargetCurrency] = useState<Currency>(currencies[1]); // Состояние для выбранной целевой валюты.
-  const [showDescription, setShowDescription] = useState<boolean>(false); // Состояние для отображения описания (не используется в данном коде).
-  const [exchangeRate, setExchangeRate] = useState<number>(0.011); // Состояние для значения обменного курса.
-
-  // Функция для обновления курса валют с сервера
   const fetchExchangeRate = () => {
     axios
       .get(
         `http://localhost:5000/prices?PaymentCurrency=${sourceCurrency.code}&PurchasedCurrency=${targetCurrency.code}`
       )
-      .then((response) => {
-        const exchangeRate = response.data.Price;
-        setExchangeRate(exchangeRate); // Обновляем состояние exchangeRate с новым значением курса.
+      .then((response: AxiosResponse) => {
+        const exchangeRate = response.data[response.data.length-1].price;
+        //console.log(exchangeRate);
+        console.log(response.data);
+        setExchangeRate(exchangeRate);
+        
         const calculatedAmount = isNaN(parseFloat(sourceAmount)) ? 0 : parseFloat(sourceAmount) * exchangeRate;
-        // Вычисляем новое значение для целевой суммы на основе исходной суммы и обменного курса.
         if (!isNaN(calculatedAmount)) {
-          setTargetAmount(calculatedAmount); // Обновляем состояние targetAmount с новой рассчитанной суммой.
+          setTargetAmount(calculatedAmount);
         }
       })
       .catch((error) => {
-        console.error('Ошибка при получении данных с сервера:', error); // Обрабатываем ошибки при получении данных с сервера.
+        console.error('Ошибка при получении данных с сервера:', error);
       });
   };
 
-  // Загружаем курс валют с сервера и устанавливаем интервал обновления
   useEffect(() => {
-    fetchExchangeRate(); // Загружаем курс валют с сервера при монтировании компонента.
-
-    // Устанавливаем интервал обновления данных каждые 10 секунд
+    fetchExchangeRate();
+    //каждые 10 секунд
     const interval = setInterval(fetchExchangeRate, 10000);
-    // Устанавливаем интервал для периодического обновления курса каждые 10 секунд.
+    return () => clearInterval(interval);
+  }, [sourceCurrency, targetCurrency]);
 
-    // Очистить интервал при размонтировании компонента
-    return () => clearInterval(interval); // Очищаем интервал при размонтировании компонента.
-  }, [sourceCurrency, targetCurrency]); // Мониторим изменения исходной и целевой валюты, чтобы обновить курс при их изменении.
-
-  // Обработчик изменения значения исходной суммы
   const handleSourceAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const amount = parseFloat(event.target.value);
-    setSourceAmount(isNaN(amount) ? '0' : amount.toString()); // Обновляем состояние sourceAmount с новым значением исходной суммы.
-
+    setSourceAmount(isNaN(amount) ? '0' : amount.toString()); 
+    //console.log(sourceAmount);
     const calculatedAmount = isNaN(amount) ? 0 : amount * exchangeRate;
-    // Вычисляем новое значение для целевой суммы на основе новой исходной суммы и обменного курса.
+    console.log(exchangeRate);
     if (!isNaN(calculatedAmount)) {
-      setTargetAmount(calculatedAmount); // Обновляем состояние targetAmount с новой рассчитанной суммой.
+      setTargetAmount(calculatedAmount);
     }
   };
 
-  // Обработчик изменения значения целевой суммы
   const handleTargetAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const amount = parseFloat(event.target.value);
-    setTargetAmount(isNaN(amount) ? 0 : amount); // Обновляем состояние targetAmount с новым значением целевой суммы.
-
+    setTargetAmount(isNaN(amount) ? 0 : amount);
     const calculatedAmount = isNaN(amount) ? 0 : amount / exchangeRate;
-    // Вычисляем новое значение для исходной суммы на основе новой целевой суммы и обменного курса.
     if (!isNaN(calculatedAmount)) {
-      setSourceAmount(calculatedAmount.toString()); // Обновляем состояние sourceAmount с новой рассчитанной суммой.
+      setSourceAmount(calculatedAmount.toString());
     }
   };
 
-  // Обработчик изменения выбранной валюты
   const handleCurrencyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedCode = event.target.value;
     const selectedCurrency = currencies.find((currency) => currency.code === selectedCode);
-    // Находим объект выбранной валюты по коду валюты из выпадающего списка.
     if (selectedCurrency) {
-      setSourceCurrency(selectedCurrency); // Обновляем состояние sourceCurrency с новым значением выбранной валюты.
+      setSourceCurrency(selectedCurrency);
     }
   };
 
-  // Обработчик клика на кнопку "Перевести"
-  const handleConvertClick = () => {
-    const calculatedAmount = parseFloat(sourceAmount) * exchangeRate;
-    // Вычисляем новое значение для целевой суммы на основе исходной суммы и обменного курса.
-    if (!isNaN(calculatedAmount)) {
-      setTargetAmount(calculatedAmount); // Обновляем состояние targetAmount с новой рассчитанной суммой.
-    }
-  };
-
-  // Возвращаем JSX разметку компонента
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', minHeight: '100vh' }}>
       <h2 style={{ margin: '0', marginBottom: '0.5rem', textAlign: 'left' }}>
@@ -137,8 +111,7 @@ const CurrencyConverterForm: React.FC<CurrencyConverterFormProps> = ({ currencie
           ))}
         </select>
       </div>
-      <button onClick={handleConvertClick}>Перевести</button>
-      <button onClick={fetchExchangeRate}>Обновить</button>
+      <button onClick={fetchExchangeRate}>Перевести или обновить</button>
     </div>
   );
 };
